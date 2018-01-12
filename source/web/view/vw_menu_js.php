7ac8293cf64e2952b88fb13e30f329574680e43a
kawakami <tkawakami@infxf.com>
2018-01-12 11:10:12 +0900

    [fix]コミット漏れ

--- source/web/view/vw_menu_js.php
+++ source/web/view/vw_menu_js.php
@@ -1,5 +1,5 @@
 <script type="text/javascript" src="./js/inpCheckFromat.js?ver=20171227_00"></script>
-<script type="text/javascript" src="./js/input.js?ver=20171227_00"></script>
+<script type="text/javascript" src="./js/input.js?ver=20180112_00"></script>
 <script type="text/javascript" src="./js/specialKey.js?ver=20171213_00"></script>
 <script type="text/javascript" src="./js/backButton.js?ver=20170808"></script>
 <script type="text/javascript" src="./js/menuShortcut.js?ver=20170525"></script>
